8829ffe55f234fd65b45505afd2468bf7bd08b11
Resul1341 <resul1341@gmail.com>
2026-05-09 17:40:05 +0300

    spring boot projesi eklendi

---
+++ src/main/java/com/example/hastanerandevusistemi/HastaneRandevuSistemiApplication.java
@@ -0,0 +1,13 @@
+package com.example.hastanerandevusistemi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HastaneRandevuSistemiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HastaneRandevuSistemiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hastanerandevusistemi/HastaneRandevuSistemiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hastanerandevusistemi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HastaneRandevuSistemiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
