77f46014f26fa6cac8ecd6dd6e0e6c577a12b737
Resul1341 <resul1341@gmail.com>
2026-05-16 01:26:13 +0300

    hastanerandevusistemiApplication.java ismi HastaneRandevuSistemiApplication.java olarak değiştirildi

--- src/main/java/com/example/hastanerandevusistemi/HastaneRandevuSistemiApplication.java
+++ src/main/java/com/example/hastanerandevusistemi/HastaneRandevuSistemiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class hastanerandevusistemiApplication {
+public class HastaneRandevuSistemiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(hastanerandevusistemiApplication.class, args);
+        SpringApplication.run(HastaneRandevuSistemiApplication.class, args);
     }
 
 }
